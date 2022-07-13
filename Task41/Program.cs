3d19f232dea009c4e42cd5d779ac970d922a0de2
Nadezhda <nbykova79@gmail.com>
2022-07-13 22:04:48 +0300

    save code task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// считает кол-во положительных чисел, введенных пользователем
+Console.WriteLine("введите кол-во чисел");
+int N = Convert.ToInt32(Console.ReadLine());
+int CountPositive(int num)
+{
+int count = 0;
+int number = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write("Введите число  ");
+        number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0) { count++;}
+    }
+    return count;
+}
+Console.WriteLine($"Вы ввели {CountPositive(N)} положительных чисел");
